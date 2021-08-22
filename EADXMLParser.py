3a403182be178ae7e26aa9ffa3a6f8d5094ad4c8
Abisola Olawale <aolawale@stanford.edu>
2021-08-22 17:11:08 -0500

    adding files for finding aid parsing tool

---
+++ EADXMLParser.py
@@ -0,0 +1,251 @@
+import xml.etree.ElementTree as ET
+import datetime
+import re
+import csv
+import glob
+import math
+import os
+from collections import defaultdict
+import spacy
+import requests
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError
+import string
+
+class EADParser:
+
+    def __init__(self):
+        self.wikidata_xml_mapping = {}
+        self.namespace = ""
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def clear_dict(self):
+        self.wikidata_xml_mapping = {k : '' for k in self.wikidata_xml_mapping}
+
+    def __get_namespace(self, element):
+        m = re.match(r'\{.*\}', element.tag)
+        return m.group(0) if m else ''
+
+    def batch_parse_xml(self, files):
+        parsed = []
+        if len(files) > 0: 
+            step = 10**math.floor(math.log10(len(files))) 
+        else: 
+            step = 1
+        for i, file in enumerate(files):
+            if i % step == 0:
+                print('%s out of %s parsed' % (i, len(files)))
+            parsed_file = self.parse_xml(file)
+            parsed.append(parsed_file)
+        print('%s out of %s parsed' % (len(files), len(files)))
+        return parsed
+    
+    def batch_parse_xml_by_reference(self, files): 
+        parsed = []
+        if len(files) > 0: 
+            step = 10**math.floor(math.log10(len(files))) 
+        else: 
+            step = 1
+        for i, file in enumerate(files):
+            if i % step == 0:
+                print('%s out of %s parsed' % (i, len(files)))
+            parsed_file = self.parse_xml_by_reference(file)
+            parsed.append(parsed_file)
+        print('%s out of %s parsed' % (len(files), len(files)))
+        return parsed
+    
+    def parse_xml_by_reference(self, file): 
+        if file:
+            self.clear_dict()
+            xmlTree = ET.parse(file)
+            root = xmlTree.getroot()
+            self.namespace = self.__get_namespace(root)
+            self.get_description(root)
+            self.get_title_and_label(root)
+            self.get_inventory_number(root, file)
+            self.get_collection_creator()
+            self.get_collection_size(root)
+            self.get_url(file)
+            res = self.wikidata_xml_mapping
+        return res
+
+    def parse_xml(self, filepath):
+        res = {}
+        ext = os.path.splitext(filepath )[-1].lower()
+        if ext == '.xml': 
+            self.clear_dict()
+            xmlTree = ET.parse(filepath)
+            root = xmlTree.getroot()
+            self.namespace = self.__get_namespace(root)
+
+            self.get_description(root)
+            self.get_title_and_label(root)
+            self.get_inventory_number(root, filepath)
+            self.get_collection_creator()
+            self.get_collection_size(root)
+            self.get_url(filepath)
+            res = self.wikidata_xml_mapping
+        return res      
+
+    def get_alt_description(self, root):
+        alt_desc = ".//%sscopecontent" % (self.namespace)
+        alt_desc_tag = root.find(alt_desc)
+        if (alt_desc_tag is not None and len(alt_desc_tag) > 0):
+            alt_desc_tag = root.findall(alt_desc)
+            description = []
+            for t in alt_desc_tag:
+                for node in t.iter():
+                    description.append(node.text)
+            return description[2]
+        return ''
+
+    def get_description(self, root):
+        abstract = './/%sabstract' % (self.namespace)
+        abstract_tag = root.findall(abstract)
+        description = ""
+        print('ABSTRACT TAG: ', abstract_tag)
+        for t in abstract_tag:
+            for node in t.iter():
+                print('NODE TEXT: ', node.text)
+                description += node.text
+        description = description.strip()
+        print("INITIAL DESCRIPTION: ", description)
+        description = description.replace('\n', '').replace('\r', '')
+        if description == "":
+            description = self.get_alt_description(root)
+        self.wikidata_xml_mapping['full_description'] = description
+        first_sentence = ''
+        doc = self.nlp(description)
+        for sent in doc.sents:
+            first_sentence = sent
+            break
+        self.wikidata_xml_mapping['description'] = first_sentence
+        print('LOOK HERE: ', description, first_sentence)
+        return (description, first_sentence)
+
+    def get_title_and_label(self, root):
+        unittitle = './/%sunittitle' % (self.namespace)
+        title_tag = root.findall(unittitle)
+        res = []
+        for t in title_tag:
+            for i, node in enumerate(list(t.iter())):
+                res.append(node.text)
+        title = res[0].translate(str.maketrans('','', string.punctuation))
+        self.wikidata_xml_mapping['title'] = title
+        self.wikidata_xml_mapping['label'] = title
+        return title
+
+    def get_alt_inventory_number(self, filepath):
+        path = filepath.upper()
+        pattern = 'ARS-?.?\d+'
+        match = re.findall(pattern, path)
+        match = match[0].replace('-', '')
+        match = match.replace('.', '')
+        return match
+
+    def get_inventory_number(self, root, filepath):
+        unitid = './/%sunitid' % (self.namespace)
+        inventory_tag = root.findall(unitid)
+        res = []
+        for t in inventory_tag:
+            for node in t.iter():
+                res.append(node.text)
+        if len(res) > 0:
+            inventory_number = res[0].replace('-', '')
+            inventory_number = inventory_number.replace(".", "")
+            self.wikidata_xml_mapping["inventory_number"] = inventory_number
+        else:
+            self.wikidata_xml_mapping['inventory_number'] = self.get_alt_inventory_number(filepath)
+        return self.wikidata_xml_mapping['inventory_number']
+
+    def get_date_retrieved(self):
+        date = datetime.datetime.now()
+        self.wikidata_xml_mapping['date_retrieved'] = date.strftime('%d %B %Y')
+        return self.wikidata_xml_mapping['date_retrieved']
+
+    def get_alt_collection_creator(self):
+        title = str(self.wikidata_xml_mapping['description'])
+        doc = self.nlp(title)
+        possible_creator = ''
+        nameFound = False
+        for ent in doc.ents:
+            if ent.label_ in ['PERSON', 'ORG']:
+                # prioritize people over orgs, choose first person found
+                if nameFound and ent.label_ == 'ORG': continue
+                if ent.label_ == 'PERSON' and nameFound == False:
+                    possible_creator = ent.text.title()
+                    nameFound = True
+                    return possible_creator
+                possible_creator = ent.text
+        return possible_creator
+
+    def get_collection_creator(self):
+        self.wikidata_xml_mapping['collection_creator'] = ''
+        title = self.wikidata_xml_mapping['title']
+        title = title.replace('Collection', '')
+        title = title.replace('The', '')
+        title = title.replace('Sheet', '')
+        title = title.replace('Music', '')
+        andInTitle = 'and' in title
+        doc = self.nlp(title.strip())
+        possible_creators = []
+        nameFound = False
+        for ent in doc.ents:
+            if ent.label_ in ['PERSON', 'ORG']:
+                # prioritize names over orgs for collection creators
+                if nameFound and ent.label_ == 'ORG': continue
+                if ent.label_ == 'PERSON': nameFound = True
+                possible_creators.append(ent.text.title())
+        if len(possible_creators) == 1:
+            self.wikidata_xml_mapping['collection_creator'] = possible_creators[0]
+        else:
+            if andInTitle and len(possible_creators) > 0:
+                self.wikidata_xml_mapping['collection_creator'] = ' and '.join(possible_creators)
+            elif len(possible_creators) > 0:
+                self.wikidata_xml_mapping['collection_creator'] = possible_creators[0]
+
+        if self.wikidata_xml_mapping['collection_creator'] == '':
+            self.wikidata_xml_mapping['collection_creator'] = self.get_alt_collection_creator()
+        return self.wikidata_xml_mapping['collection_creator']
+
+    def get_collection_size(self, root):
+        physdesc =  './/%sphysdesc' % (self.namespace)
+        physdesc_tag = root.findall(physdesc)
+        pattern = '\\n\s*'
+        outer_res = []
+        for t in physdesc_tag:
+            inner_res = []
+            for node in t.iter():
+                inner_res.append(node.text)
+            outer_res.append(inner_res)
+
+        collection_size = ' '.join(outer_res[0])
+        collection_size = re.sub('\\n\s*', '', collection_size)
+        self.wikidata_xml_mapping['collection_size'] = collection_size
+        return collection_size
+
+    def get_url(self, filepath):
+        finding_aid_url = ''
+        name = self.wikidata_xml_mapping['label']
+        querified = name.replace(" ", "%20")
+        url = "https://searchworks.stanford.edu/?q=" + querified + "&format=json"
+        try:
+            response = requests.get(url=url)
+            data = response.json()
+        except (ConnectTimeout, HTTPError, ReadTimeout, Timeout, ConnectionError):
+            pass
+
+        if len(data['response']['docs']) > 0:
+            try:
+                finding_aid_url = data['response']['docs'][0]['url_suppl'][0]
+            except KeyError:
+                pass
+
+        self.wikidata_xml_mapping['url'] = finding_aid_url
+        return finding_aid_url
+  
+    def combine_parsed_files(self, parsed): 
+        final_dict = defaultdict(list)
+        for item in parsed:
+            for k, v in item.items(): 
+                final_dict[k].append(v)
+        return final_dict
---
+++ EADXMLParserFrontend.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd 
+import base64
+
+from EADXMLParser import * 
+import os
+import xml.etree.ElementTree as ET
+
+from random import randint
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    return href
+
+def main():
+    if 'refresh_files' not in st.session_state: 
+        st.session_state['refresh_files'] = False 
+    parser = EADParser()   
+    st.title('Parse XML Files')
+    menu = ["Parse File(s)"]
+    xml_files = None
+    st.subheader("Parse file(s)")
+    multiple_files = st.file_uploader("Select a single file or multiple files. Refresh the page to clear all files.", accept_multiple_files=True)
+
+    if multiple_files is None:
+        st.text("No files selected. Please select at least one file.") 
+    else: 
+        uploaded_files = [file for file in multiple_files]
+        xml_files = [file for file in uploaded_files if os.path.splitext(file.name)[1] == ".xml"]
+        non_xml_files = [file.name for file in uploaded_files if os.path.splitext(file.name)[1] != ".xml"]
+        
+        if len(non_xml_files) > 0: 
+            invalid_file_warning = "Found non xml files (these will be ignored): " + ", ".join(non_xml_files)
+            st.warning(invalid_file_warning)
+
+        if st.button("Parse Files"): 
+            if len(xml_files) > 0: 
+                has_parsed_files = True
+                parsed = []
+                progress_bar = st.progress(0)
+                step = 1/len(xml_files)
+                amount_done = 0
+
+                for i, file in enumerate(xml_files):
+                    amount_done += step
+                    parsed_file = parser.parse_xml_by_reference(file)
+                    parsed.append(parsed_file)
+                    progress_bar.progress(amount_done)
+                
+                combined_files = parser.combine_parsed_files(parsed)
+                df = pd.DataFrame.from_dict(combined_files)
+                st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+            else: 
+                st.warning("No .xml files detected! Please double check selected file(s) and ensure the extension on each file is .xml.")
+        
+
+if __name__ == '__main__': 
+    main()
